f63c005db34e20106b81eab84c0edbfe43f69a6d
Charles Baker <charles.baker@sweetsoftware.co.nz>
2013-11-12 17:19:37 +1300

    Add mat4x4 Lua bindings.

--- math_lua/LuaMath.cpp
+++ math_lua/LuaMath.cpp
@@ -7,6 +7,7 @@
 #include "LuaVec2.hpp"
 #include "LuaVec3.hpp"
 #include "LuaQuat.hpp"
+#include "LuaMat4x4.hpp"
 #include <stddef.h>
 
 using namespace sweet;
@@ -15,15 +16,18 @@ using namespace sweet::math;
 LuaMath::LuaMath( lua::Lua& lua )
 : lua_vec2_( NULL ),
   lua_vec3_( NULL ),
-  lua_quat_( NULL )
+  lua_quat_( NULL ),
+  lua_mat4x4_( NULL )
 {
     lua_vec2_ = new math::LuaVec2( lua );
     lua_vec3_ = new math::LuaVec3( lua );
     lua_quat_ = new math::LuaQuat( lua );
+    lua_mat4x4_ = new math::LuaMat4x4( lua );
 }
 
 LuaMath::~LuaMath()
 {
+    delete lua_mat4x4_;
     delete lua_quat_;
     delete lua_vec3_;
     delete lua_vec2_;
@@ -44,3 +48,7 @@ math::LuaQuat* LuaMath::lua_quat() const
     return lua_quat_;
 }
 
+math::LuaMat4x4* LuaMath::lua_mat4x4() const
+{
+    return lua_mat4x4_;
+}
--- math_lua/LuaMath.hpp
+++ math_lua/LuaMath.hpp
@@ -17,12 +17,14 @@ namespace math
 class LuaVec2;
 class LuaVec3;
 class LuaQuat;
+class LuaMat4x4;
 
 class LuaMath
 {
     math::LuaVec2* lua_vec2_;
     math::LuaVec3* lua_vec3_;
     math::LuaQuat* lua_quat_;
+    math::LuaMat4x4* lua_mat4x4_;
 
 public:
     LuaMath( lua::Lua& lua );
@@ -30,6 +32,7 @@ public:
     math::LuaVec2* lua_vec2() const;
     math::LuaVec3* lua_vec3() const;
     math::LuaQuat* lua_quat() const;
+    math::LuaMat4x4* lua_mat4x4() const;
 };
 
 }
