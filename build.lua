3ce0d9cee9eb44c6f3d823a1d0f48d7637123b09
Charles Baker <charles.baker@sweetsoftware.co.nz>
2018-08-12 14:11:03 +1200

    Rename “build” to “forge” in build scripts and buildfiles

--- build.lua
+++ build.lua
@@ -1,39 +1,39 @@
 
-require 'build';
-require 'build.cc';
-require 'build.visual_studio';
-require 'build.xcode';
-require 'build.macos';
-require 'build.windows';
+require 'forge';
+require 'forge.cc';
+require 'forge.visual_studio';
+require 'forge.xcode';
+require 'forge.macos';
+require 'forge.windows';
 
-platform = platform or build:operating_system();
+platform = platform or forge:operating_system();
 variant = variant or 'debug';
 version = version or ('%s %s %s'):format(os.date('%Y.%m.%d %H:%M:%S'), platform, variant );
 goal = goal or "";
 jobs = jobs or 4;
 
-build:initialize {
+forge:initialize {
     variants = { 'debug', 'release', 'shipping' };
-    bin = build:root( ('%s/bin'):format(variant) );
-    lib = build:root( ('%s/lib'):format(variant) );
-    obj = build:root( ('%s/obj'):format(variant) );
+    bin = forge:root( ('%s/bin'):format(variant) );
+    lib = forge:root( ('%s/lib'):format(variant) );
+    obj = forge:root( ('%s/obj'):format(variant) );
     include_directories = {
-        build:root( '..' );
-        build:root( 'src/lua/src' );
-        build:root( 'src/unittest-cpp' );
+        forge:root( '..' );
+        forge:root( 'src/lua/src' );
+        forge:root( 'src/unittest-cpp' );
     };
     library_directories = {
-        build:root( ('%s/lib'):format(variant) );
+        forge:root( ('%s/lib'):format(variant) );
     };
     visual_studio = {
-        sln = build:root( 'math.sln' );
+        sln = forge:root( 'math.sln' );
     };
     xcode = {
-        xcodeproj = build:root( 'math.xcodeproj' );
+        xcodeproj = forge:root( 'math.xcodeproj' );
     };
 };
 
-build:default_targets {
+forge:default_targets {
     'math_lua',
     'math_test'
 };
